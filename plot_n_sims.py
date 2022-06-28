66b34ba45df8150a784f0750225e28b67a0c8618
Kusha Sareen <kushasareen42@gmail.com>
2022-06-28 00:19:05 -0400

    More efficient plotting

--- plot_n_sims.py
+++ plot_n_sims.py
@@ -11,11 +11,8 @@ if __name__ == "__main__":
 	max_score = max([x[0] for x in results])
 	print("The max score was", max_score)
 
-	flip_lists = [x[1] for x in results]
-	score_lists = [x[2] for x in results]
-
 	for i in range(n):
-		plt.plot(score_lists[i], flip_lists[i])
+		plt.plot(results[i][2], results[i][1])
 
 	plt.xlabel("Score")
 	plt.ylabel("Flips")
